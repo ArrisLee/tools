fc8eb37ecc8097c1b0036d89165d28fdfd3ed067
ArrisLee <arris_li@hotmail.com>
2021-03-17 00:22:39 +1300

    init

---
+++ fcommit/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func main() {
+	c := exec.Command("git", "add", "--all")
+	out, err := c.Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Print(string(out))
+	c = exec.Command("git", "commit", "-m", "init")
+	out, err = c.Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Print(string(out))
+	c = exec.Command("git", "push", "origin", "master")
+	out, err = c.Output()
+	if err != nil {
+		log.Println(err)
+	}
+	log.Print(string(out))
+}
