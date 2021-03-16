7b5170bb40644ca4a374d0c1fdbd23ac1497d829
ArrisLee <arris_li@hotmail.com>
2021-03-17 00:43:51 +1300

    fcommit update

--- fcommit/main.go
+++ fcommit/main.go
@@ -13,11 +13,17 @@ func main() {
 	args := flag.Args()
 	commitMsg := strings.Join(flag.Args()[:len(args)-1], " ")
 	branch := args[len(args)-1]
+	// fmt.Println(commitMsg)
+	// fmt.Println(branch)
+	// fmt.Println(fmt.Sprintf("git push origin %s", branch))
 	c := exec.Command("bash", "-c", "git add --all")
+	log.Println("adding...")
 	c.Run()
 	c = exec.Command("bash", "-c", fmt.Sprintf("git commit -m '%s'", commitMsg))
+	log.Println("committing...")
 	c.Run()
 	c = exec.Command("bash", "-c", fmt.Sprintf("git push origin %s", branch))
+	log.Println("pushing...")
 	out, err := c.Output()
 	if err != nil {
 		log.Fatal(err)
